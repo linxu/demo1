47d9b836302ab2c25ea7dcc218188e0e74a43293
林旭 <alfred.lin@soundbus.cn>
2018-08-18 20:21:04 +0800

    update

--- src/views/index/index.js
+++ src/views/index/index.js
@@ -1,6 +1,6 @@
 import cache from "../../libs/cache";
 import * as app from "../../libs/app";
-import {QNRTCSession, deviceManager} from 'pili-rtc-web';
+import {deviceManager, QNRTCSession} from 'pili-rtc-web';
 
 app.sendCrashReport();
 
@@ -17,18 +17,22 @@ var vm = new Vue({
         const localVideo = document.getElementById('localplayer');
         const remoteVideo = document.getElementById('remoteplayer');
         const myRTC = new QNRTCSession();
+        myRTC.addListener('user-leave', function (user) {
+            app.info('user leave');
+            app.info(user);
+        });
         myRTC.joinRoomWithToken("QxZugR8TAhI38AiJ_cptTl3RbzLyca3t-AAiH-Hh:ciuERtjohxEa0as5s3N79LnVPPs=:eyJhcHBJZCI6ImQ4bGs3bDRlZCIsInJvb21OYW1lIjoiaGVsbG8iLCJ1c2VySWQiOiJ0ZXN0MTIzIiwiZXhwaXJlQXQiOjE1MzQ2NzIyOTQwNjMxMzk3NDgsInBlcm1pc3Npb24iOiJ1c2VyIn0=\n").then(function (users) {
             app.info('current users', users);
             deviceManager.getLocalStream({
-                video: { enabled: true },
-                audio: { enabled: true },
+                video: {enabled: true},
+                audio: {enabled: true},
             }).then(function (stream) {
                 stream.play(localVideo, true);
                 myRTC.publish(stream);
             }).catch(function (e) {
                 app.error("stream play error!", e);
             })
-            for (let i = 0; i < users.length; i +=1) {
+            for (let i = 0; i < users.length; i += 1) {
                 app.info(i);
                 const user = users[i];
                 if (user.published && user.userId !== myRTC.userId) {
